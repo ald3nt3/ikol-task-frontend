b3bd405d23dcda72e5c6ecb3f7e4476169194273
ald3nt3 <33810885+ald3nt3@users.noreply.github.com>
2025-01-30 03:18:53 +0100

    input group component

---
+++ src/components/Group.vue
@@ -0,0 +1,35 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <fieldset>
+    <legend>{{ title }}</legend>
+    <slot></slot>
+  </fieldset>
+</template>
+
+<style>
+legend {
+  position: absolute;
+  top: -0.5rem;
+  line-height: 1rem;
+  left: 0.5rem;
+  padding: 0 0.5rem;
+  background-color: var(--color-background);
+}
+fieldset {
+  position: relative;
+  padding: 1.5rem 1rem 1rem 1rem;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid var(--color-border);
+  background: var(--color-background);
+  border-radius: 8px;
+}
+</style>
